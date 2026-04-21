40cbe921e4d9ff101dc8c012037aef06fd6fea32
Yjasik <motoligin1@gmail.com>
2026-04-21 09:36:33 +0300

    Migrate to Foundry, restructure contracts, remove old files

--- contracts/contracts/PredictionMarketFactory.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-import "@openzeppelin/contracts/access/AccessControl.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "./PredictionMarket.sol";
-
